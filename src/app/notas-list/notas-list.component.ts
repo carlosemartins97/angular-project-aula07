4f83c61d628c975be7daa9e88f3d6329c4456fba
carlosemartins97 <carlosemartins97@gmail.com>
2021-09-11 10:42:01 -0300

    2 files modified

--- src/app/notas-list/notas-list.component.ts
+++ src/app/notas-list/notas-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Nota } from './nota';
 
 @Component({
@@ -7,14 +8,41 @@ import { Nota } from './nota';
   styleUrls: ['./notas-list.component.css']
 })
 export class NotasListComponent implements OnInit {
-  listaNotas: Nota[] = [
-    { name: 'Carlos', nota: 100 },
-    { name: 'Joaquim', nota: 55 },
-    { name: 'Joana', nota: 72 },
-    { name: 'Cleo', nota: 90 }
-  ];
+  listaNotas: Nota[] = [];
+  isEditMode = false;
+
+  nota: number;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.listaNotas = this.getData();
+  }
+
+  getData(): Nota[] {
+    const savedValues = localStorage.getItem('notas');
+    if (savedValues) {
+      return JSON.parse(savedValues);
+    } else {
+      this.listaNotas.push(
+        { name: 'Carlos', nota: 100 },
+        { name: 'Joaquim', nota: 55 },
+        { name: 'Joana', nota: 72 },
+        { name: 'Cleo', nota: 90 }
+      );
+      localStorage.setItem('notas', JSON.stringify(this.listaNotas));
+      return this.listaNotas;
+    }
+  }
+
+  onChange(v: string, index: number) {
+    const data = this.getData();
+    const value = Number(v);
+    for (let i = 0; i < data.length; i++) {
+      if (index === i) {
+        data[i].nota = value;
+        localStorage.setItem('notas', JSON.stringify(data));
+      }
+    }
+  }
 }
