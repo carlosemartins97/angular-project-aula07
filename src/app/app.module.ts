ceff030fb5b9c0df87d521bc5e701d5fd45fd0ad
carlosemartins97 <carlosemartins97@gmail.com>
2021-09-11 09:58:47 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRouteModule } from './app-route/app-route.module';
 import { CommonModule } from '@angular/common';
+import { QuadroDisciplinasComponent } from './quadro-disciplinas/quadro-disciplinas.component';
+import { DisciplinasListComponent } from './disciplinas-list/disciplinas-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRouteModule, CommonModule],
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent,
+    DisciplinasListComponent,
+    QuadroDisciplinasComponent,],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
